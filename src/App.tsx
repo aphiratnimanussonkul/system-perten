fba58c1df16be51fa214ca19bde8e12e7cb96ec5
Aphirat Nimanussonkul <b5923151@gmail.com>
2022-07-30 16:39:23 +0700

    calculate

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,54 @@ import { useEffect, useState } from "react";
 import { getTippings } from "./services/tipping.service";
 
 function App() {
+  const [betSelected, setBetSelected] = useState<boolean[][]>([]);
   const [tipping, setTipping] = useState<ITipping[]>([]);
+  const [betNumber, setBetNumber] = useState(0);
+
   useEffect(() => {
-    getTippings().then(setTipping);
+    getTippings().then((data) => {
+      setTipping(data);
+      const arrayColumn = Array.from(Array(3)).map(() => false);
+      let initArray = Array.from(Array(data.length));
+      initArray = initArray.map(() => [...arrayColumn]);
+      setBetSelected([...initArray]);
+    });
   }, []);
 
-  const buildRowData = ({ data }: { data: ITipping }) => {
+  useEffect(() => {
+    calculateBet();
+  }, [betSelected]);
+
+  const onSelectBet = (row: number, column: number) => {
+    betSelected[row][column] = !betSelected[row][column];
+    setBetSelected([...betSelected]);
+  };
+
+  const calculateBet = () => {
+    if (betSelected.every((el) => el.some((check) => check))) {
+      let rowBet = [0, 0, 0];
+      for (const row of betSelected) {
+        const count = row.filter((check) => check).length;
+        rowBet[count - 1]++;
+      }
+      console.log(rowBet);
+      setBetNumber(
+        Math.pow(1, rowBet[0]) * Math.pow(2, rowBet[1]) * Math.pow(3, rowBet[2])
+      );
+    } else setBetNumber(0);
+  };
+
+  const buildRowData = ({ data, index }: { data: ITipping; index: number }) => {
+    const BuildBetColumn = ({ columnIndex }: { columnIndex: number }) => {
+      return (
+        <td
+          className="border cursor-pointer"
+          onClick={() => onSelectBet(index, columnIndex)}
+        >
+          {betSelected[index][columnIndex] ? "X" : ""}
+        </td>
+      );
+    };
     return (
       <tr className="border">
         <td className="border">{data.nr}</td>
@@ -22,6 +64,9 @@ function App() {
         <td className="border">{data.tips.expertHomeTip}</td>
         <td className="border">{data.tips.expertDrawTip}</td>
         <td className="border">{data.tips.expertAwayTip}</td>
+        <BuildBetColumn columnIndex={0} />
+        <BuildBetColumn columnIndex={1} />
+        <BuildBetColumn columnIndex={2} />
       </tr>
     );
   };
@@ -32,12 +77,15 @@ function App() {
           <tr className="border">
             <th className="border">R</th>
             <th className="border">Antall Rekker</th>
-            <th className="border">{0}</th>
+            <th className="border">{betNumber}</th>
             <th colSpan={3} className="border">
               Folket %
             </th>
             <th colSpan={3} className="border">
               Ekspert %
+            </th>
+            <th colSpan={3} className="border">
+              Stamme
             </th>
           </tr>
           <tr className="border">
@@ -50,9 +98,14 @@ function App() {
             <th className="border">H</th>
             <th className="border">U</th>
             <th className="border">B</th>
+            <th className="border">H</th>
+            <th className="border">U</th>
+            <th className="border">B</th>
           </tr>
         </thead>
-        <tbody>{tipping.map((data) => buildRowData({ data }))}</tbody>
+        <tbody>
+          {tipping.map((data, index) => buildRowData({ data, index }))}
+        </tbody>
       </table>
     </div>
   );
